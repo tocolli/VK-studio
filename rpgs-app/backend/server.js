3860d4151a1cc530452d2476cb1e55b123914d72
tocolli <tvitorbarbosa@gmail.com>
2026-03-06 11:27:28 -0300

    agora vai2

--- rpgs-app/backend/server.js
+++ rpgs-app/backend/server.js
@@ -31,14 +31,8 @@ if (!fs.existsSync(uploadDir)) {
 }
 app.use('/uploads', express.static(uploadDir));
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, uploadDir);
-    },
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + path.extname(file.originalname));
-    }
-});
+
+const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 // Substitua o createConnection por createPool
